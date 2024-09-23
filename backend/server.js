e5bfe3923794bc9b932eca6d91305810f73a68ea
Copeta1 <pecolajrobert1@gmail.com>
2024-09-23 11:00:59 +0200

    Backend

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
+import productRoutes from "./routes/product.route.js";
+
+
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.get("/products ", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use(express.json());
 
-app.listen(3001, () => {
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started on http://localhost/3001");
+  console.log("Server started on http://localhost:" + PORT);
 });
