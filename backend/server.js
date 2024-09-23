689c977e5480d22a816a04890140e17973630e49
Copeta1 <pecolajrobert1@gmail.com>
2024-09-23 09:14:32 +0200

    db

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/products ", (req, res) => {
   res.send("Hello, World!");
 });
 
 app.listen(3001, () => {
+  connectDB();
   console.log("Server started on http://localhost/3001");
 });
-proba;
